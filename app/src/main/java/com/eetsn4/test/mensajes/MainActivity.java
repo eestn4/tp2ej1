4340cd08ce3ca137b106e19c1fbad2f0dbc2c0e7
Alberto Paparelli (a.k.a. carpediem) <alberto@paparelli.com.ar>
2017-06-12 18:53:51 -0300

    Initial commit

---
+++ app/src/main/java/com/eetsn4/test/mensajes/MainActivity.java
@@ -0,0 +1,24 @@
+package com.eetsn4.test.mensajes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void mostrar(View v){
+        mostrar_msg();
+    }
+
+    private void mostrar_msg(){
+        Toast.makeText(this, "Este es un mensaje",
+                Toast.LENGTH_LONG).show();
+    }
+}
